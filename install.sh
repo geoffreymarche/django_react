92bdd05a5e3e5248fa9fe37eb6905822e5f19717
geoffreymarche <geoffrey.marche@themoocagency.com>
2017-08-26 19:52:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-
-echo "py lib install"
+echo "###"
+echo "### PY LIB INSTALL ###"
+echo "###"
 pip install -r requirements.txt
-echo "npm lib install"
+echo "###"
+echo "### NPM LIBS INSTALL ###"
+echo "###"
 npm install
-echo "webpacket compilation"
+echo "###"
+echo "### WEBPACK SCRIPTS COMPILATION"
+echo "###"
 ./node_modules/.bin/webpack --config webpack.config.js
-echo "/!\ the installation is over /!\"
+echo "/!\"
+echo "THE INSTALLATION IS OVER"
+echo "/!\"
+echo "### to start your application :"
+echo "### ./manage.py runserver"
+echo "### OR"
+echo "### ./manage.py runserver my-app-ip:my-app-port"
